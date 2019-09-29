62adbe48cd9ccf76a9dcb8f3e000d954a8cea73e
Dominique VASSARD <dominique.vassard@gmail.com>
2019-09-29 16:10:26 +0200

    ecto.drop removes constraints and indexes

--- lib/ecto_neo4j/storage.ex
+++ lib/ecto_neo4j/storage.ex
@@ -1,6 +1,8 @@
 defmodule Ecto.Adapters.Neo4j.Storage do
   @behaviour Ecto.Adapter.Storage
 
+  alias Ecto.Adapters.Neo4j.Cql.Node, as: NodeCql
+
   def storage_up(_) do
     :ok
   end
@@ -8,7 +10,17 @@ defmodule Ecto.Adapters.Neo4j.Storage do
   def storage_down(data) do
     Application.ensure_all_started(data[:otp_app])
 
-    Bolt.Sips.query!(Bolt.Sips.conn(), Ecto.Adapters.Neo4j.Cql.Node.delete_all())
+    conn = Bolt.Sips.conn()
+    Bolt.Sips.query!(conn, NodeCql.delete_all())
+
+    [NodeCql.list_all_constraints("", nil), NodeCql.list_all_indexes("", nil)]
+    |> Enum.map(fn cql ->
+      Bolt.Sips.query!(conn, cql)
+      |> Map.get(:records, [])
+    end)
+    |> List.flatten()
+    |> Enum.map(&NodeCql.drop_constraint_index_from_cql/1)
+    |> Enum.map(&Bolt.Sips.query(conn, &1))
 
     :ok
   end
