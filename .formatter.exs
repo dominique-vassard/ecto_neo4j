24a4c0603ba828145d072f56f66acebe76c3a49e
Dominique VASSARD <dominique.vassard@gmail.com>
2020-01-03 11:22:06 +0100

    Add formater

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,15 @@
+locals_without_parens = [
+  outgoing_relationship: 2,
+  outgoing_relationship: 3,
+  incoming_relationship: 2,
+  incoming_relationship: 3
+]
 # Used by "mix format"
 [
   import_deps: [:ecto],
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  locals_without_parens: locals_without_parens,
+  export: [
+    locals_without_parens: locals_without_parens
+  ]
 ]
