6af6770fd9d16db808170da84f9cff1e1daf7753
Maximilly M <max.ocw@gmail.com>
2021-03-07 21:57:50 -0300

    default test.exs content

--- config/test.exs
+++ config/test.exs
@@ -2,7 +2,12 @@ use Mix.Config
 
 config :bolt_sips, Bolt,
   hostname: 'localhost',
-  basic_auth: [username: "neo4j", password: "channel-dialog-classic-nerve-mexico-8725"],
+  basic_auth: [username: "neo4j", password: "test"],
   port: 7687,
   pool_size: 5,
   max_overflow: 1
+
+config :ecto_neo4j, Ecto.Adapters.Neo4j,
+  chunk_size: 10_000,
+  batch: false,
+  bolt_role: :direct
